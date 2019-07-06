2bc40893f57d7eff973d6bfe5125a75b8d0946c4
vicpril <vicpride@gmail.com>
2019-07-06 20:22:17 +0700

    done

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require("jquery");
 require("bootstrap/dist/js/bootstrap.bundle");
+// require("@fortawesome/fontawesome-free/js/all.js");
+// require("@fortawesome/fontawesome-free/css/all.css");
 
 import "./scss/style.scss";
 // import "./scss/bootstrap.scss";
